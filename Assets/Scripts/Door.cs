9a619d11ae4486cbc169d3030e8322bf7f772d87
Simon Hembert <simhembert@gmail.com>
2019-04-29 23:46:45 +0200

    réparation porte et sons

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,17 +5,32 @@ public class Door : MonoBehaviour
     [SerializeField]
     private Animator doorAnimator = null;
 
+    [SerializeField]
+    private AudioClip openClip = null;
+
+    [SerializeField]
+    private AudioClip closeClip = null;
+
     public bool isOpened;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void Open()
     {
         doorAnimator.SetTrigger("open");
+        audioSource.PlayOneShot(openClip);
         isOpened = true;
     }
 
     public void Close()
     {
         doorAnimator.SetTrigger("close");
+        audioSource.PlayOneShot(closeClip);
         isOpened = false;
     }
 }
